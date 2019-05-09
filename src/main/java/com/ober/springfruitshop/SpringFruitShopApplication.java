8f8187c3a89f43f43ab55c9a8f8cb2b7c8841f9c
Sam Ober <ober.sam@gmail.com>
2019-05-09 15:34:14 -0400

    Initial commit

---
+++ src/main/java/com/ober/springfruitshop/SpringFruitShopApplication.java
@@ -0,0 +1,13 @@
+package com.ober.springfruitshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFruitShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFruitShopApplication.class, args);
+    }
+
+}
